1ee1db271f5e4742b8b5c717c2ef6e9340f5e2b7
Remso27 <kouassiremi27@gmail.com>
2022-07-02 02:33:22 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int y, z;
+
+	y = 97;
+	z = 65;
+
+	while (y < 123)
+	{
+		putchar(y);
+		y++;
+	}
+	while (z < 97)
+	{
+		putchar(z);
+		z++;
+	}
+	potchar(10)
+}
+
+
