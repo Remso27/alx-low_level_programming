259fd3a6a7acabab09ab15cab8022e1e5361123b
Remso27 <kouassiremi27@gmail.com>
2022-07-02 04:40:19 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int x;
+
+	x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
