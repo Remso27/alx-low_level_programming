dad1e791383503daeeb064e2329c9de55627c0a8
Remso27 <kouassiremi27@gmail.com>
2022-07-05 16:39:32 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+	char letter = "a";
+	
+	while (x <= 'z')
+	{
+		_putchar(letter);
+		letter++
+	}
+	_putchar('\n');
+}
