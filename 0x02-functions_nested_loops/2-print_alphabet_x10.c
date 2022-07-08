a0725b10927ec4ca1df01c3c7137dbcc848af003
Remso27 <kouassiremi27@gmail.com>
2022-07-08 10:13:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for (x = '0'; x <= 10; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+		_putchar (i);
+		}
+	_putchar ('\n');
+	}
+}
