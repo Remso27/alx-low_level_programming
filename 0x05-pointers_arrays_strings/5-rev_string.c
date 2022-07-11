4548b9d57abbcff54fc0e2effcda0ace7aacbeb9
Remso27 <kouassiremi27@gmail.com>
2022-07-11 11:09:22 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int x, max, half;
+	char first, last
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	max = x - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
