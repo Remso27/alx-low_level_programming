3d65e451dfc457fc87d3a638b3b51c67ce5366a9
Remso27 <kouassiremi27@gmail.com>
2022-07-14 10:06:40 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,8 +14,8 @@ int *_strcmp(char *s1, char *s2)
 
 	while (y == 0)
 	{
-		if ((*(s1 + x) == '\0' && (*(s2 + x) == '\0')
-					break;
+		if ((*(s1 + x) == '\0') && (*(s2 + x) == '\0'))
+			break;
 		y = *(s1 + x) - *(s2 + x);
 		x++;
 }
